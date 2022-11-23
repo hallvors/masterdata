a728af903bdd72b21a5bc61ce70129e5a2e5c19d
Hallvord R. M. Steen <hallvord@minus.no>
2022-11-23 13:56:19 +0100

    add challenge evaluation schema

---
+++ masterdata/schemas/challenge_evaluation.js
@@ -0,0 +1,54 @@
+export default {
+    type: "object",
+    name: "challenge_evaluation",
+    title: "Evaluering",
+    fields: [
+        {
+            name: "challenge",
+            title: "Didaktisk utfordring",
+            type: "reference",
+            to: [{ type: "didactic_challenge" }],
+          },
+          {
+            name: "score",
+            type: "number",
+            title: "Klassifisering",
+            description:
+              "0: brukes negativt - motarbeider målet, 1: har ingen effekt,  2: brukes svakt, 3: brukes vellykket",
+          },
+          {
+            name: "requires_teacher",
+            type: "boolean",
+            title: "Krever lærerinnsats",
+            description: "I følge lærerveiledning er affordansen beregnet på å brukes som grunnlag for lærer-styrte aktiviteter",
+          },
+          {
+            name: "comment",
+            type: "text",
+            title: "Kommentar",
+          },
+          {
+            name: "screenshot",
+            type: "image",
+            title: "Skjermdump",
+          },
+    ],
+    preview: {
+        select: {
+          title: "challenge.title",
+          //subtitle: "challenge.title",
+          media: "screenshot",
+          score: "score",
+        },
+        prepare: (data) => {
+          if (
+            data.score !== undefined &&
+            data.title &&
+            data.title.indexOf(" - " + data.score) === -1
+          ) {
+            data.title += " - " + data.score;
+          }
+          return data;
+        },
+      },
+}
