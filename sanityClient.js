c5bfa37841d45438775abb4ca1ae2bc2b4c7fb1e
Hallvord R. M. Steen <hallvord@minus.no>
2023-02-12 14:21:39 +0100

    Remove tokens from Git

--- sanityClient.js
+++ sanityClient.js
@@ -1,15 +1,18 @@
+require('dotenv').config()
 
 const createSanityClient = require('@sanity/client');
 const sanityClient = createSanityClient({
-    token: 'skAUKzIqAQD1UQJwc2ZciHn6rOpog9gDeKkZFi6UHD4oNy6IZX4AevhDX4n23h2boRo2KK8mOstIJUcIa7f4y9hlWWuqBfKVxX8oM2yJugx4SzkyIraMEMEXKni1RztUOnXRNulKzt2kutYVS3I4OApiFaHSgnWCXgX97nA9AOu1dLWpJsXx',
+    token: process.env.SANITY_TOKEN_AUNIVERS,
     projectId: 'fbbshc6p',
     dataset: 'aunivers',
     apiVersion: '2022-10-30',
     useCdn: false,
   });
 
+sanityClient.sanityClientAunivers = sanityClient;
+  
 sanityClient.sanityClientSkolestudio = createSanityClient({
-    token: 'sk5skxKwUSIjecv2q0cnnYZnjInUUJgXo05XZnw6F6Qfjxu2QtloMleWml1A6w0WNo9ZbXa8yi8uGHpiy8TI1DPRvHUT159hlnZDpQ99myX2Of46p2KRj1GIqJ8iJibuuQh5hMfVht8z284tzruLbm4MGezezf4npc2TJgMwIa7uH5nJCIxs',
+    token: process.env.SANITY_TOKEN_SKOLESTUDIO,
     projectId: '7xashn1s',
     dataset: 'skolestudio',
     apiVersion: '2022-10-30',
@@ -17,7 +20,7 @@ sanityClient.sanityClientSkolestudio = createSanityClient({
   });
 
 sanityClient.sanityClientSkolenMin = createSanityClient({
-    token: 'skAUKzIqAQD1UQJwc2ZciHn6rOpog9gDeKkZFi6UHD4oNy6IZX4AevhDX4n23h2boRo2KK8mOstIJUcIa7f4y9hlWWuqBfKVxX8oM2yJugx4SzkyIraMEMEXKni1RztUOnXRNulKzt2kutYVS3I4OApiFaHSgnWCXgX97nA9AOu1dLWpJsXx',
+    token: process.env.SANITY_TOKEN_SKOLENMIN,
     projectId: 'fbbshc6p',
     dataset: 'skolen-cdu',
     apiVersion: '2022-10-30',
